7d5e54014380b25d2d13bd74872ef2b5a065f23f
Dmitrii <lifanov.dv93@gmail.com>
2021-07-14 16:59:13 +0300

    Модернизировал AM для запуска ТК в разных браузерах

--- saucedemo-web-tests/src/test/java/com/saucedemo/appmanager/ApplicationManager.java
+++ saucedemo-web-tests/src/test/java/com/saucedemo/appmanager/ApplicationManager.java
@@ -3,6 +3,11 @@ package com.saucedemo.appmanager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.opera.OperaOptions;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,15 +19,29 @@ public class ApplicationManager {
     private NavigationHelper navigationHelper;
     private OrderHelper orderHelper;
     private String baseUrl;
-
+    private String browserType = BrowserType.CHROME;
     private StringBuffer verificationErrors = new StringBuffer();
 
     public void init() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        wd = new ChromeDriver();
+        switch (browserType) {
+            case "OPERA":
+                OperaOptions oo = new OperaOptions();
+                oo.addArguments("--headless");
+                wd = new OperaDriver();
+                break;
+            case "FIREFOX":
+                FirefoxOptions fo = new FirefoxOptions();
+                fo.addArguments("--headless");
+                wd = new FirefoxDriver();
+                break;
+            default:
+                ChromeOptions co = new ChromeOptions();
+                co.addArguments("--headless");
+                wd = new ChromeDriver();
+                break;
+        }
         baseUrl = "https://www.google.com/";
-        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         orderHelper = new OrderHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
