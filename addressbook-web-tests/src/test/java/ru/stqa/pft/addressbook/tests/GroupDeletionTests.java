6828c066957a6b1ee07c61d4b7e2bfa3ad1e09b9
Dmitrii <lifanov.dv93@gmail.com>
2021-07-20 14:15:56 +0300

    Добавлены проверки на количество групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -9,11 +10,17 @@ public class GroupDeletionTests extends TestBase {
     public void testGroupDeletion() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
         if (!app.getGroupHelper().isGroupPresent()) {
-          app.getGroupHelper().createGroup(new GroupData("123", null, null));
+            int before = app.getGroupHelper().getGroupCount();
+            app.getGroupHelper().createGroup(new GroupData("123", null, null));
+            int after = app.getGroupHelper().getGroupCount();
+            Assert.assertEquals(after, before + 1);
         }
+        int before = app.getGroupHelper().getGroupCount();
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before - 1);
     }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -9,12 +10,18 @@ public class GroupModificationTests extends TestBase {
     public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
         if (!app.getGroupHelper().isGroupPresent()) {
+            int before = app.getGroupHelper().getGroupCount();
             app.getGroupHelper().createGroup(new GroupData("123", null, null));
+            int after = app.getGroupHelper().getGroupCount();
+            Assert.assertEquals(after, before + 1);
         }
+        int before = app.getGroupHelper().getGroupCount();
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("1234", "1234", "1234"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before);
     }
 }
