fcfda744fe36a621fa6db9ad15b5bd190cd4b13c
Dmitrii <lifanov.dv93@gmail.com>
2021-07-20 10:46:56 +0300

    Оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -3,21 +3,37 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper extends HelperBase{
+public class NavigationHelper extends HelperBase {
 
     public NavigationHelper(WebDriver wd) {
         super(wd);
     }
 
     public void gotoGroupPage() {
-      click(By.linkText("groups"));
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))) {
+            return;
+        } else {
+            click(By.linkText("groups"));
+        }
     }
 
     public void gotoAddContactPage() {
-        click(By.linkText("add new"));
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry")
+                && isElementPresent(By.name("submit"))) {
+            return;
+        } else {
+            click(By.linkText("add new"));
+        }
     }
 
     public void gotoHomePage() {
-        click(By.linkText("home"));
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        } else {
+            click(By.linkText("home"));
+        }
     }
 }
