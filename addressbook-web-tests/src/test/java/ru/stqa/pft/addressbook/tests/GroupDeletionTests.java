a6e275a49a7ee9ffc3a7ed6caaf7214aaeb6060c
Dmitrii <lifanov.dv93@gmail.com>
2021-07-21 08:02:24 +0300

    Оптимизация получения количества веб элементов (групп) через коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -4,23 +4,25 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupDeletionTests extends TestBase {
 
     @Test
     public void testGroupDeletion() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
         if (!app.getGroupHelper().isGroupPresent()) {
-            int before = app.getGroupHelper().getGroupCount();
+            List<GroupData> before = app.getGroupHelper().getGroupList();
             app.getGroupHelper().createGroup(new GroupData("123", null, null));
-            int after = app.getGroupHelper().getGroupCount();
-            Assert.assertEquals(after, before + 1);
+            List<GroupData> after = app.getGroupHelper().getGroupList();
+            Assert.assertEquals(after.size(), before.size() + 1);
         }
-        int before = app.getGroupHelper().getGroupCount();
-        app.getGroupHelper().selectGroup(before - 1);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before - 1);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() - 1);
     }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,24 +4,26 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupModificationTests extends TestBase {
 
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
         if (!app.getGroupHelper().isGroupPresent()) {
-            int before = app.getGroupHelper().getGroupCount();
+            List<GroupData> before = app.getGroupHelper().getGroupList();
             app.getGroupHelper().createGroup(new GroupData("123", null, null));
-            int after = app.getGroupHelper().getGroupCount();
-            Assert.assertEquals(after, before + 1);
+            List<GroupData> after = app.getGroupHelper().getGroupList();
+            Assert.assertEquals(after.size(), before.size() + 1);
         }
-        int before = app.getGroupHelper().getGroupCount();
-        app.getGroupHelper().selectGroup(before - 1);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("1234", "1234", "1234"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size());
     }
 }
